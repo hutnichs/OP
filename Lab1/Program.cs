23c2fba4dd68df78377652ec3b17202b1498ce77
hutnichs <hutnichs@gmail.com>
2026-02-27 18:26:48 +0200

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        TimeoutColorIterator.ConsumeWithTimeout(
+            CyclicDateGenerator.GenerateDaysOfWeek(),
+            5
+        );
+    }
+}
