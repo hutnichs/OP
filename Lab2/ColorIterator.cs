1a1747678d5b3db8bdb3e944a4b43ed0b215d6f5
hutnichs <hutnichs@gmail.com>
2026-03-01 10:04:52 +0200

    Add files via upload

---
+++ Lab2/ColorIterator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public static class TimeoutColorIterator
+{
+    public static void ConsumeWithTimeout<T>(
+        IEnumerable<T> source,
+        int timeoutSeconds)
+    {
+        ConsoleColor[] colors =
+        {
+            ConsoleColor.Red,
+            ConsoleColor.Yellow,
+            ConsoleColor.Green,
+            ConsoleColor.Cyan,
+            ConsoleColor.Blue,
+            ConsoleColor.Magenta,
+            ConsoleColor.White
+        };
+
+        int colorIndex = 0;
+        var stopwatch = Stopwatch.StartNew();
+
+        foreach (var item in source)
+        {
+            if (stopwatch.Elapsed.TotalSeconds >= timeoutSeconds)
+                break;
+
+            Console.ForegroundColor = colors[colorIndex];
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} - {item}");
+
+            colorIndex = (colorIndex + 1) % colors.Length;
+        }
+
+        Console.ResetColor();
+    }
+}
