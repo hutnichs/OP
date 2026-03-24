dd6f82b11e24d65cd6d255edee80e70898926578
hutnichs <hutnichs@gmail.com>
2026-03-24 18:41:53 +0200

    added function

--- Lab3/src/memo.js
+++ Lab3/src/memo.js
@@ -18,3 +18,11 @@ function memoize(fn, { maxSize = Infinity } = {}) {
     return result;
   };
 }
+function factorial(n) {
+  if (n < 0) return null;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+const memoFactorial = memoize(factorial, { maxSize: 50 });
+console.log(memoFactorial(5));
