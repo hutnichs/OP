5fa2d268178fb251d90350b1535c6b40986666f7
hutnichs <hutnichs@gmail.com>
2026-04-15 16:08:49 +0300

    Update EvEm

--- Lab7/EvEm.js
+++ Lab7/EvEm.js
@@ -1,38 +1,20 @@
-class EventEmitter {
+export default class EventEmitter {
   constructor() {
     this.events = new Map();
   }
-    subscribe(event, listener) {
+  subscribe(event, listener) {
     if (!this.events.has(event)) this.events.set(event, new Set());
-    this.events.get(event).add(listener);
+    const listeners = this.events.get(event);
+    listeners.add(listener);
+    return () => {
+      listeners.delete(listener);
+      if (listeners.size === 0) this.events.delete(event);
+    };
   }
-    emit(event, payload) {
+  emit(event, payload) {
     if (!this.events.has(event)) return;
     for (const listener of this.events.get(event)) {
       listener(payload);
     }
   }
 }
-class Producer {
-  constructor(emitter) {
-    this.emitter = emitter;
-  }
-  send(data) {
-    this.emitter.emit("message", data);
-  }
-}
-class Consumer {
-  constructor(name, emitter) {
-    this.name = name;
-    emitter.subscribe("message", this.handle.bind(this));
-  }
-    handle(data) {
-    console.log(this.name, "received:", data);
-  }
-}
-const emitter = new EventEmitter();
-const producer = new Producer(emitter);
-const consumerA = new Consumer("A", emitter);
-const consumerB = new Consumer("B", emitter);
-producer.send({ value: 1 });
-producer.send({ value: 2 });
