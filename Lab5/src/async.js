ec5e18ee91b9088d214945b8685c593027485f74
hutnichs <hutnichs@gmail.com>
2026-04-15 20:05:41 +0300

    exports

--- Lab5/src/async.js
+++ Lab5/src/async.js
@@ -1,58 +1,7 @@
-function asyncMapCallback(array, asyncFn, done) {
-  const results = new Array(array.length);
-  let completed = 0;
-  let failed = false;
-
-  array.forEach((item, index) => {
-    asyncFn(item, index)
-      .then((res) => {
-        if (failed) return;
-
-        results[index] = res;
-        completed++;
-
-        if (completed === array.length) {
-          done(null, results);
-        }
-      })
-      .catch((err) => {
-        if (!failed) {
-          failed = true;
-          done(err, null);
-        }
-      });
-  });
-}
-
-function asyncMapPromise(array, asyncFn) {
-  return new Promise((resolve, reject) => {
-    const results = new Array(array.length);
-    let completed = 0;
-    let failed = false;
-
-    array.forEach((item, index) => {
-      asyncFn(item, index)
-        .then((res) => {
-          if (failed) return;
-
-          results[index] = res;
-          completed++;
-
-          if (completed === array.length) {
-            resolve(results);
-          }
-        })
-        .catch((err) => {
-          if (!failed) {
-            failed = true;
-            reject(err);
-          }
-        });
-    });
-  });
-}
+const asyncMapPromise = require("./asyncMapPromise");
+const asyncMapCallback = require("./asyncMapCallback");
 
 module.exports = {
-  asyncMapCallback,
   asyncMapPromise,
+  asyncMapCallback
 };
